7d0029f2fbdff89c9848f533c07dd4d5321d4a12
CHENEAU LEO <leo.cheneau@etu.unistra.fr>
2021-02-16 14:39:36 +0100

    Ajout de la cible coverage à CMake. Mode par défaut CMake = Debug

---
+++ include/coverage.h
@@ -0,0 +1,10 @@
+/* 
+ * Fichier de test pour le coverage 
+ * Merci de supprimer ce fichier dès que vouds aurez des vraies sources !!!
+ */
+#ifndef INCLUDE_COVERAGE__H
+#define INCLUDE_COVERAGE__H
+
+int foo();
+
+#endif
---
+++ src/coverage.cpp
@@ -0,0 +1,8 @@
+/*
+ * Fichier de test pour le coverage
+ * Merci de supprimer ce fichier dès que vouds aurez des vraies sources !!!
+ */
+int foo()
+{
+	return 0;
+}
